f7ecbeebfa40cb792de12e724034703f17d67c94
prithviya <94467336+prithviya@users.noreply.github.com>
2022-11-30 10:07:13 +0530

    chairman mesage changes done

--- src/components/Academic/academic.js
+++ src/components/Academic/academic.js
@@ -69,18 +69,30 @@ function academic() {
                                 </span>
                             </h1>
                             <div class="uk-panel">
-                                <img class="uk-align-left uk-margin-remove-adjacent"  src={Images.CHAIRMAN} width="350" height="300" alt="Example_academic"/>
+                                <img class="uk-align-left uk-margin-remove-adjacent"  src={Images.CHAIRMAN} width="350" height="320" alt="Example_academic"/>
                                 <p className='uk-text-justify'>
-                                    When you walk through the portals of Rathinam, you are entering into a world that leads to your future. We believe that acquiring a degree is only a part of the learning process. The bigger quest is to prepare the students mentally to lead their lives better. 
+                                    "A good leader leads the people from above them. A great leader leads the people from within them."
                                 </p>
                                 <p className='uk-text-justify'>
-                                    At Rathinam, our goal is to create global leaders who will contribute their knowledge in building a more prosperous and peaceful world. We are committed to inculcate strong, ethical and moral values to our students. Our faculty members prepare the students to become self-reliant.
+                                    A visionary & philanthropist, who presaged to create an abode of learning, following the trails of his parents’ dream by transforming it into reality by sculpting a magnificent consortium to serve society in terms of education, employment and community living.
                                 </p>
                                 <p className='uk-text-justify'>
-                                Finally, I have a message to share, “Do everything to keep the name and fame of the RATHINAM  high”.
+                                    Dr. Madan A Sendhil, Chairman of Rathinam Group is the mastermind behind the growth and progress of Rathinam group in the field of education, innovation, entrepreneurship and various enterprises. He is the leading light of Rathinam Group of Institutions, Rathinam IT Parks, Rathinam Shelters, SEZ and CEO of Atal Incubation Centre – Raise, India’s first Incubation Centre focusing on United Nations Sustainable Development Goals. Rathinam has risen to greater heights with his vision to bring a reformation in all these fields with his hardworking, selfless and prudent management.
                                 </p>
                                 <p className='uk-text-justify'>
-                                Wishing you the best of everything.
+                                    He is a veteran with strong educational background, having completed his bachelor’s degree in Electronics & Communication Engineering from Coimbatore Institute of Technology in 1994. He went on to graduate Masters in Computer Engineering from the University of Central Florida in 1996 and relentlessly pursued Ph.D in Information & Communication Engineering from Anna University, Chennai in 2019. From being an educationist, he accelerated his skill in entrepreneurship and IT field with such expertise that he was awarded the title “Indian Power Entrepreneur” by Coimbatore Football Association, “Thannambikkai Star” by Thannambikkai Magazine and has been called the Technopreneur for his contribution in establishing SEZ in Rathinam.
+                                </p>
+                                <p className='uk-text-justify'>
+                                    From a humble beginning, he has grown by perseverance to work for various noble causes and envisions building a strong student community, endowed with innovative, creative, critical and forward-thinking professionals. He aspires to prepare a powerful student community who will be self-reliant, ethically and morally strong citizens for an ingenious, prosperous and peaceful world.
+                                </p>
+                                <p className='uk-text-justify'>
+                                    Under his leadership, Rathinam Group of education has earned superior accreditations such as NAAC and Autonomous status that has elevated Rathinam as one of the illustrious institutions in India.
+                                </p>
+                                <p className='uk-text-justify'>
+                                    As an educationist and an entrepreneur, Dr. Madan has been lauded for his contribution; the UK Parliament has honoured him with the prestigious award- “The Leading Edupreneur of India”, in recognition of his service amongst the top-notch Indian businessmen.  
+                                </p>
+                                <p className='uk-text-justify'>
+                                    As the Chairman of Rathinam Group, he spurs the same dynamism and exhilarates his characteristics to the Rathinam family with an absolute attitude to contribute to society through service in education & business.
                                 </p>
                             </div>
                         </li>
@@ -93,19 +105,19 @@ function academic() {
                             <div class="uk-panel">
                                 <img class="uk-align-left uk-margin-remove-adjacent"  src={Images.PRINCIPAL} width="350" height="200" alt="Example_academic"/>
                                     <p className='uk-text-justify'>
-                                            Welcome to Rathinam College of Physiotherapy (RCP), an institution that rests on a strong academic foundation, merged with modern approaches that mould youth into successful professionals. RCP is well known for its commitment towards excellence in every aspect. It is a blend of competent academicians, state of the art infrastructure, experienced and efficient management, safe and convenient environment to provide perfect balance in academics, sports and social opportunities for the students.
+                                        Welcome to Rathinam College of Physiotherapy (RCP), an institution that rests on a strong academic foundation, merged with modern approaches that mould youth into successful professionals. RCP is well known for its commitment towards excellence in every aspect. It is a blend of competent academicians, state of the art infrastructure, experienced and efficient management, safe and convenient environment to provide perfect balance in academics, sports and social opportunities for the students.
+                                    </p>
+                                <p className='uk-text-justify'>
+                                    Our aim is to make students learning an enriching, pleasant and fulfilling experience, combining theory and practical’s in pedagogy. Focus is made not only on ‘what is taught’ but also ‘the way it is taught’. We strive to make our students good human beings and also prestigious professionals through multiple teaching strategies during their journey with us.
                                 </p>
                                 <p className='uk-text-justify'>
-                                Our aim is to make students learning an enriching, pleasant and fulfilling experience, combining theory and practical’s in pedagogy. Focus is made not only on ‘what is taught’ but also ‘the way it is taught’. We strive to make our students good human beings and also prestigious professionals through multiple teaching strategies during their journey with us.
-                                </p>
-                                <p className='uk-text-justify'>
-                                I am extremely proud of the professionalism, commitment, dedication and teamwork of our well experienced staffs to deliver excellent education for students in a caring and supportive environment. I believe in upholding high education standards with an absolute commitment to improve the student education process, contributing to their holistic development. Students have to keep in mind that their success is our achievement.
+                                    I am extremely proud of the professionalism, commitment, dedication and teamwork of our well experienced staffs to deliver excellent education for students in a caring and supportive environment. I believe in upholding high education standards with an absolute commitment to improve the student education process, contributing to their holistic development. Students have to keep in mind that their success is our achievement.
                                 </p>
                                 <p className='uk-text-justify'>
                                     Choosing the right institution to enrol your son/daughter is the best investment in your life and is a significant decision for the long term. As put forward by Nelson Mandela, “Education is the most powerful weapon which you can use to change the world.” With good education, we can alter the society for the better, contributing to our Nation’s progress too.
                                 </p>
                                 <p className='uk-text-justify'>
-                                I wish all the students a grand success in their career. We are here to support you to achieve your dreams and come out with flying colours!!!!!
+                                    I wish all the students a grand success in their career. We are here to support you to achieve your dreams and come out with flying colours!!!!!
                                 </p>
                             </div>
                         </li>
